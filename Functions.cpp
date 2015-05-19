29c7886f46527bd5d4a7714d9a71ee87f8d1de2b
Vladimir Strisovsky <vladimir.strisovsky@kistler.com>
2015-05-19 09:22:25 +0200

    Inital commit

--- Functions.cpp
+++ Functions.cpp
@@ -9,193 +9,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <tuple>
+#include <vector>
+#include <typeinfo>
 
-template <typename T>
-struct function_traits;
+#include <boost/timer.hpp>
 
-template <typename T>
-struct function_traits;
+#include "functions.h"
 
-// For generic types, directly use the result of the signature of its 'operator()'
-
-template <typename ClassType, typename ReturnType, typename... Args>
-struct function_traits<ReturnType(ClassType::*)(Args...) const>
-// we specialize for pointers to member function
+struct A
 {
-    enum { arity = sizeof...(Args) };
-    // arity is the number of arguments.
-
-    typedef ReturnType result_type;
-
-    template <size_t i>
-    struct arg
+    int value() const
     {
-        typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
-        // the i-th argument is equivalent to the i-th tuple element of a tuple
-        // composed of those arguments.
-    };
+        return x * 5;
+    }
+
+    int x;
 };
-
-enum NodeTypeEnum
-{
-    eGenerator,
-    eFunctor,
-    eTerminator
-};
-
-template<int _Type>
-struct NodeType
-{
-};
-
-template<typename _Left, typename _Right, typename _Enable = void>
-struct _Connection
-{
-    typedef typename _Left::_OutType _InType;
-    typedef typename _Right::_InType _OutType;
-
-    _Left _l;
-    _Right _r;
-
-    _Connection(_Left l, _Right r)
-    : _l(l), _r(r)
-    {
-    }
-
-//    _OutType apply(_InType value)
-//    {
-//        _r(value);
-//    }
-};
-
-template<typename _Left, typename _Right>
-struct _Connection<_Left, _Right, typename std::enable_if<std::is_same<NodeType<eGenerator>, typename _Left::_NodeType>::value >::type>
-{
-    typedef typename _Left::_OutType _InType;
-    typedef typename _Right::_InType _OutType;
-
-
-    _Connection(_Left l, _Right r)
-    {
-//        while(l._node.next())
-//        {
-//            r.apply(l._node.value());
-//        }
-    }
-
-};
-
-template<typename _T>
-struct _Node
-{
-    typedef typename _T::_InType _InType;
-    typedef typename _T::_OutType _OutType;
-    typedef typename _T::_NodeType _NodeType;
-
-    _T _node;
-
-    _Node(_T node)
-    : _node(node)
-    {
-    }
-};
-
-template<typename _Left, typename _Right>
-_Connection<_Node<_Left>, _Node<_Right>> operator>>(_Node<_Left> l, _Node<_Right> r)
-{
-    return _Connection<_Node<_Left>, _Node<_Right>>(l,r);
-}
-
-template<typename _T>
-struct _Range
-{
-    typedef void _InType;
-    typedef _T _OutType;
-    typedef NodeType<eGenerator> _NodeType;
-
-    _T _current, _to, _value;
-    _Range(_T first, _T last)
-    : _current(first), _to(last), _value(first)
-    {
-    }
-
-    bool next()
-    {
-        bool result = _current < _to;
-        _value = _current;
-        ++_current;
-        return result;
-    }
-
-    _T value() const
-    {
-        return _value;
-    }
-};
-
-template<typename _T>
-_Node<_Range<_T>> range(_T first, _T last)
-{
-    return _Node<_Range<_T>>(_Range<_T>(first, last));
-}
-
-template<typename _Fnc>
-struct _Terminator
-{
-    typedef function_traits<decltype(&_Fnc::operator())> _InTraits;
-    typedef typename _InTraits::template arg<0>::type _InType;
-    typedef void _OutType;
-    typedef NodeType<eTerminator> _NodeType;
-
-    _Fnc _fnc;
-    _Terminator(_Fnc fnc)
-    : _fnc(fnc)
-    {
-
-    }
-
-    void operator()(_InType v)
-    {
-        _fnc(v);
-    }
-};
-
-template<typename _Fnc>
-struct _Functor
-{
-    typedef function_traits<decltype(&_Fnc::operator())> _InTraits;
-    typedef typename _InTraits::template arg<0>::type _InType;
-    typedef typename _InTraits::result_type _OutType;
-    typedef NodeType<eFunctor> _NodeType;
-
-    _Fnc _fnc;
-    _Functor(_Fnc fnc)
-    : _fnc(fnc)
-    {
-
-    }
-
-    _OutType operator()(_InType v)
-    {
-        _fnc(v);
-    }
-};
-
-template<typename _Fnc>
-_Node<_Functor<_Fnc>> Map(_Fnc fnc)
-{
-    return _Node<_Functor<_Fnc>>(_Functor<_Fnc>(fnc));
-}
-
-template<typename _Fnc>
-_Node<_Terminator<_Fnc>> ForEach(_Fnc fnc)
-{
-    return _Node<_Terminator<_Fnc>>(fnc);
-}
 
 int main(void)
 {
-    range(0, 100) >> Map([](int i){ return 2 * i;}) >> ForEach([](int i){std::cout << "value: " << i << "\n";});
+    boost::timer tm;
+
+    std::vector<int> v = {1,3,5,67,33,56,44,45,67,76,67,789};
+
+    Functions _;
+
+    _(   From({9,8,7,6,5,4,3,2,1,0})
+     ,   Filter([](int i) {return (i % 2) == 0;})
+     ,   Map([](int i){return i+i;})
+     ,   ForEach([](int i){std::cout << i << "\n";})
+     );
+    std::cout << "------\n";
+    _(   Range(0, 1000)
+     ,   Filter([](int i) {return (i % 2) == 0;})
+     ,   Take(20)
+     ,   Map([](int i){return i+i;})
+     ,   ForEach([](int i){std::cout << i << "\n";})
+     );
+    std::cout << "------\n";
+    _(   Range(0)
+     ,   Zip(From(v))
+     ,   Zip(Range(100))
+     ,   ForEach([](std::tuple<int, int, int>&& v){std::cout << std::get<0>(v) << ":" << std::get<1>(v) << ":" << std::get<2>(v) << "\n";})
+     );
+    std::cout << "------\n";
+    _(   From({A{2000}, A{3}})
+     ,   Map(&A::value)
+     ,   Filter([](int i){return i > 10;})
+     ,   ForEach([](int i){std::cout << i << "\n";})
+     );
+    std::cout << "------\n";
+    _(   From({A{2000}, A{3}})
+     ,   Map(&A::x)
+     ,   Filter([](int i){return i > 10;})
+     ,   ForEach([](int i){std::cout << i << "\n";})
+     );
+    std::cout << "------\n";
+    _(   Range(0, 1000)
+     ,   Drop(998)
+     ,   ForEach([](int i){std::cout << i << "\n";})
+     );
+    std::cout << "------\n";
+
+    std::cout << tm.elapsed() << "\n";
+
     return EXIT_SUCCESS;
 }
---
+++ include/functions.h
@@ -0,0 +1,395 @@
+#ifndef INCLUDE_FUNCTIONS_H_
+#define INCLUDE_FUNCTIONS_H_
+
+#include <tuple>
+#include <initializer_list>
+#include <type_traits>
+#include <limits>
+
+struct Empty{};
+
+template<int I>
+struct Int
+{
+    static const int i = I;
+
+    typedef Int<i-1> Prev;
+    typedef Int<i+1> Next;
+};
+
+enum class StepType
+{
+    eGenerator,
+    eTransformator,
+    eFilter,
+    eLimiter,
+    eTerminator
+};
+
+template<typename _T>
+struct _Range
+{
+    static const StepType stepType = StepType::eGenerator;
+    typedef Empty InType;
+    typedef _T OutType;
+    _T _start, _end;
+    _Range(_T start, _T end)
+    : _start(start), _end(end)
+    {
+    }
+
+    bool hasNext() const
+    {
+        return _end > _start;
+    }
+
+    _T value()
+    {
+        return _start++;
+    }
+};
+
+template<typename _T>
+_Range<_T> Range(_T start, _T end = std::numeric_limits<_T>::max())
+{
+    return _Range<_T>(start, end);
+}
+
+template<typename _T>
+struct _FromValueType
+{
+    typedef typename _T::value_type type;
+};
+
+template<typename _T>
+struct _FromValueType<const _T*>
+{
+    typedef _T type;
+};
+
+template<typename _T>
+struct _From
+{
+    static const StepType stepType = StepType::eGenerator;
+    typedef Empty InType;
+    typedef typename _FromValueType<_T>::type OutType;
+    _T _start, _end;
+    _From(_T start, _T end)
+    : _start(start), _end(end)
+    {
+    }
+
+    bool hasNext() const
+    {
+        return _start != _end;
+    }
+
+    OutType value()
+    {
+        return *(_start++);
+    }
+};
+
+template<typename _T>
+_From<typename _T::const_iterator> From(const _T& coll)
+{
+    return _From<typename _T::const_iterator>(std::begin(coll), std::end(coll));
+}
+
+template<typename _T>
+_From<_T> From(_T start, _T end)
+{
+    return _From<_T>(start, end);
+}
+
+template<typename _T>
+_From<typename std::initializer_list<_T>::const_iterator> From(std::initializer_list<_T>&& coll)
+{
+    return _From<typename std::initializer_list<_T>::const_iterator>(coll.begin(), coll.end());
+}
+
+template<typename _Gen>
+struct _Zip
+{
+    static const StepType stepType = StepType::eTransformator;
+    typedef Empty InType;
+     _Gen _gen;
+    _Zip(_Gen&& gen)
+    : _gen(gen)
+    {
+    }
+
+    bool hasNext() const
+    {
+        return _gen.hasNext();
+    }
+
+    typename _Gen::OutType value()
+    {
+        return _gen.value();
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I)
+    {
+        typedef typename _I::Next Next;
+        return hasNext() ? std::get<_I::i+1>(functions).push(std::make_tuple(std::move(v), value()), functions, Next()) : false;
+    }
+
+    template<typename ... _T, typename _F, typename _I>
+    bool push(std::tuple<_T...>&& v, _F& functions, _I)
+    {
+        typedef typename _I::Next Next;
+        return hasNext() ? std::get<_I::i+1>(functions).push(std::tuple_cat(std::move(v), std::make_tuple(value())), functions, Next()) : false;
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+    }
+
+};
+
+template<typename _Gen>
+_Zip<_Gen> Zip(_Gen&& gen)
+{
+    return _Zip<_Gen>(std::move(gen));
+}
+
+template<typename _Fnc, typename _Enable = void>
+struct _Map
+{
+    static const StepType stepType = StepType::eTransformator;
+    _Fnc _fnc;
+    _Map(_Fnc&& fnc)
+    : _fnc(fnc)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I i)
+    {
+        typedef typename _I::Next Next;
+        return std::get<_I::i+1>(functions).push(_fnc(v), functions, Next());
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+        typedef typename _I::Next Next;
+        std::get<_I::i>(functions).finish(functions, Next());
+    }
+
+};
+
+template<typename _Fnc>
+struct _Map<_Fnc, typename std::enable_if<std::is_member_function_pointer<_Fnc>::value>::type>
+{
+    static const StepType stepType = StepType::eTransformator;
+    _Fnc _fnc;
+    _Map(_Fnc&& fnc)
+    : _fnc(fnc)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I i)
+    {
+        typedef typename _I::Next Next;
+        return std::get<_I::i+1>(functions).push((v.*_fnc)(), functions, Next());
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+        typedef typename _I::Next Next;
+        std::get<_I::i>(functions).finish(functions, Next());
+    }
+
+};
+
+template<typename _Fnc>
+struct _Map<_Fnc, typename std::enable_if<std::is_member_object_pointer<_Fnc>::value>::type>
+{
+    static const StepType stepType = StepType::eTransformator;
+    _Fnc _fnc;
+    _Map(_Fnc&& fnc)
+    : _fnc(fnc)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I i)
+    {
+        typedef typename _I::Next Next;
+        return std::get<_I::i+1>(functions).push((v.*_fnc), functions, Next());
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+        typedef typename _I::Next Next;
+        std::get<_I::i>(functions).finish(functions, Next());
+    }
+
+};
+
+template<typename _Fnc>
+_Map<_Fnc> Map(_Fnc&& fnc)
+{
+    return _Map<_Fnc>(std::move(fnc));
+}
+
+template<typename _Fnc>
+struct _ForEach
+{
+    static const StepType stepType = StepType::eTerminator;
+    _Fnc _fnc;
+    _ForEach(_Fnc&& fnc)
+    : _fnc(fnc)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I)
+    {
+        _fnc(std::move(v));
+        return true;
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+    }
+
+};
+
+template<typename _Fnc>
+_ForEach<_Fnc> ForEach(_Fnc&& fnc)
+{
+    return _ForEach<_Fnc>(std::move(fnc));
+}
+
+template<typename _Fnc>
+struct _Filter
+{
+    static const StepType stepType = StepType::eFilter;
+    _Fnc _fnc;
+    _Filter(_Fnc&& fnc)
+    : _fnc(fnc)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I)
+    {
+        if(_fnc(v))
+        {
+            typedef typename _I::Next Next;
+            return std::get<_I::i+1>(functions).push(std::move(v), functions, Next());
+        }
+
+        return true;
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+        typedef typename _I::Next Next;
+        std::get<_I::i>(functions).finish(functions, Next());
+    }
+};
+
+template<typename _Fnc>
+_Filter<_Fnc> Filter(_Fnc&& fnc)
+{
+    return _Filter<_Fnc>(std::move(fnc));
+}
+
+struct _Take
+{
+    static const StepType stepType = StepType::eLimiter;
+    int _count;
+
+    _Take(int count)
+    : _count(count)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I)
+    {
+        if(_count-- != 0)
+        {
+            typedef typename _I::Next Next;
+            return std::get<_I::i+1>(functions).push(std::move(v), functions, Next());
+        }
+        return false;
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+    }
+};
+
+_Take Take(int count)
+{
+    return _Take(count);
+}
+
+/////////////
+struct _Drop
+{
+    static const StepType stepType = StepType::eLimiter;
+    int _count;
+
+    _Drop(int count)
+    : _count(count)
+    {
+    }
+
+    template<typename _T, typename _F, typename _I>
+    bool push(_T&& v, _F& functions, _I)
+    {
+        if(_count != 0)
+        {
+            --_count;
+            return true;
+        }
+        typedef typename _I::Next Next;
+        return std::get<_I::i+1>(functions).push(std::move(v), functions, Next());
+    }
+
+    template<typename _F, typename _I>
+    void finish(_F& functions, _I)
+    {
+    }
+};
+
+_Drop Drop(int count)
+{
+    return _Drop(count);
+}
+/////////////
+struct Functions
+{
+    template<typename ... _T>
+    void operator()(_T&&... t)
+    {
+        typedef std::tuple<_T...> type;
+        type functions(std::forward<_T>(t)...);
+
+        bool cont = true;
+        while(cont && std::get<0>(functions).hasNext())
+        {
+            cont = std::get<1>(functions).push(std::get<0>(functions).value(), functions, Int<1>());
+        };
+        std::get<1>(functions).finish(functions, Int<1>());
+    }
+};
+
+
+
+
+#endif /* INCLUDE_FUNCTIONS_H_ */
