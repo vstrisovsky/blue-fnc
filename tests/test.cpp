048110406acfd31ba64e5c5cf2dbb83477ee25c9
Vladimir Strisovsky <vstrisovsky@gmail.com>
2015-06-25 15:45:06 +0200

    Added catch unit test framework

---
+++ tests/test.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Functions.cpp
+// Author      : Vladimir Strisovsky
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+
+#include "functions.h"
+
+
+TEST_CASE("Test", "[tag]")
+{
+  bool b = true;
+  REQUIRE( b == true );
+}
