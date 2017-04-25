4b0a89d7affb4a06745f90bb2cde16b43934fdf6
Vladimir Strisovsky <vstrisovsky@gmail.com>
2017-04-25 15:59:08 +0200

    refactored tests

---
+++ tests/repeatedly.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : range.cpp
+// Author      : Vladimir Strisovsky
+// Version     :
+// Copyright   : Your copyright notice
+// Description : unit tests for range
+//============================================================================
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+#include <sstream>
+
+#include "functions.hpp"
+#include "test_result.hpp"
+
+
+TEST_CASE("Repeatedly generators")
+{
+  Functions _;
+
+  SECTION("Return constant")
+  {
+    auto result =
+        _(   repeatedly([]{return 1;})
+         ,   take(3)
+         ,   tovector<TestResult>()
+        );
+    REQUIRE(result == TestResult({1,1,1}));
+  }
+}
+
+TEST_CASE("Repeat generators")
+{
+  Functions _;
+
+  SECTION("Return constant")
+  {
+    auto result =
+    _(   repeat(1)
+     ,   take(3)
+     ,   tovector<TestResult>()
+    );
+    REQUIRE(result == TestResult({1,1,1}));
+  }
+}
---
+++ tests/test_result.hpp
@@ -0,0 +1,9 @@
+#ifndef _TEST_RESULT_HPP_
+#define _TEST_RESULT_HPP_
+
+#include <vector>
+
+typedef std::vector<int> TestResult;
+typedef std::string TestStringResult;
+
+#endif
